d7666f897a0bdb4b18cd35554f3543ac0f262c28
reja <rezzanurbaitulloh@gmail.com>
2026-08-15 13:04:43 +0700

    fix(background): jangan render watermark saat URL kosong (hindari warning src kosong)

--- src/components/animated-background.tsx
+++ src/components/animated-background.tsx
@@ -23,13 +23,15 @@ export function AnimatedBackground() {
             transition={{ duration: 1.2 }}
             className="absolute -right-8 top-20 hidden h-[380px] w-[380px] md:block lg:h-[480px] lg:w-[480px]"
           >
-            <motion.img
-              src={watermarkPemuda}
-              alt=""
-              className="h-full w-full rounded-full object-cover opacity-[0.14] grayscale-[35%] ring-1 ring-line dark:opacity-[0.10]"
-              animate={{ y: [0, -18, 0], rotate: [-3, 3, -3] }}
-              transition={{ duration: 14, repeat: Infinity, ease: "easeInOut" }}
-            />
+            {watermarkPemuda && (
+              <motion.img
+                src={watermarkPemuda}
+                alt=""
+                className="h-full w-full rounded-full object-cover opacity-[0.14] grayscale-[35%] ring-1 ring-line dark:opacity-[0.10]"
+                animate={{ y: [0, -18, 0], rotate: [-3, 3, -3] }}
+                transition={{ duration: 14, repeat: Infinity, ease: "easeInOut" }}
+              />
+            )}
           </motion.div>
 
           <motion.div
@@ -38,13 +40,15 @@ export function AnimatedBackground() {
             transition={{ duration: 1.2, delay: 0.4 }}
             className="absolute -left-10 bottom-4 hidden h-[280px] w-[280px] lg:block xl:h-[340px] xl:w-[340px]"
           >
-            <motion.img
-              src={watermarkPemudi}
-              alt=""
-              className="h-full w-full rounded-full object-cover opacity-[0.09] grayscale-[45%] ring-1 ring-line dark:opacity-[0.07]"
-              animate={{ y: [0, 16, 0], rotate: [3, -3, 3], scale: [1, 1.03, 1] }}
-              transition={{ duration: 17, repeat: Infinity, ease: "easeInOut" }}
-            />
+            {watermarkPemudi && (
+              <motion.img
+                src={watermarkPemudi}
+                alt=""
+                className="h-full w-full rounded-full object-cover opacity-[0.09] grayscale-[45%] ring-1 ring-line dark:opacity-[0.07]"
+                animate={{ y: [0, 16, 0], rotate: [3, -3, 3], scale: [1, 1.03, 1] }}
+                transition={{ duration: 17, repeat: Infinity, ease: "easeInOut" }}
+              />
+            )}
           </motion.div>
 
           {/* Pulsa warna lembut */}
